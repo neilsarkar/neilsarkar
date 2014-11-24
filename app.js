eee01629f8df7dd16628e030beacbf3968de5088
Neil Sarkar <neil.r.sarkar@gmail.com>
2014-11-23 20:26:58 -0800

    Add more of that sweet, sweet content

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname + '/app'))
+app.use(express.static(__dirname + '/app/public'))
 app.use(express.static(__dirname + '/bower_components'))
 
 var server = app.listen(process.env.PORT || 3000, function () {
